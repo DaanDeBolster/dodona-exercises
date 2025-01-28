0489dbc857d029719ae62e3329ee98d9b8380a6b
ddbolste <104028232+DaanDeBolster@users.noreply.github.com>
2025-01-28 09:52:25 +0100

    sdfghfhjgf

--- exercises/Afstand tussen punten/solution/solution.py
+++ exercises/Afstand tussen punten/solution/solution.py
@@ -1,4 +1,7 @@
-fb = float(input())
-vb = float(input())
-cs = 343
-print(fb*cs/(cs-vb))
+from math import sqrt
+
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+print(sqrt((x2-x1)**2+(y2-y1)**2))
